6ac62806b5daf75d3f587b9b834fa3838a4f3fce
Victor Garcia <victorgf2011@gmail.com>
2024-03-08 23:17:11 +0100

    refactor(www): clean build for static module

--- src/main/kotlin/eu/tortitas/stash/plugins/Static.kt
+++ src/main/kotlin/eu/tortitas/stash/plugins/Static.kt
@@ -3,15 +3,31 @@ package eu.tortitas.stash.plugins
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
 import io.ktor.server.routing.*
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import java.io.File
 
 fun Application.configureStatic() {
-    launch {
-        ProcessBuilder("npm", "run", "build").directory(File("www")).start()
-    }
+    handleNode()
 
     routing {
         staticFiles("/", File("www/dist"))
+    }
+}
+
+fun handleNode() {
+    installNodeDependenciesAndBuild()
+    runBlocking {
+        startBuildWatcher()
+    }
+}
+
+fun installNodeDependenciesAndBuild() {
+    ProcessBuilder("npm", "install").directory(File("www")).start().waitFor()
+    ProcessBuilder("npm", "run", "build").directory(File("www")).start().waitFor()
+}
+
+suspend fun startBuildWatcher() = coroutineScope {
+    launch {
+        ProcessBuilder("npm", "run", "build:watch").directory(File("www")).start()
     }
 }
