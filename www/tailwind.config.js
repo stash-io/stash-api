d11b40422a0f631a807ce431abd5783dce4ad147
Victor Garcia <victorgf2011@gmail.com>
2024-03-11 22:43:53 +0100

    feat: first page

--- www/tailwind.config.js
+++ www/tailwind.config.js
@@ -1,30 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.html',
-  ],
+  content: ["./src/**/*.html"],
   theme: {
     fontFamily: {
-        sans: ['Poppins', 'sans-serif'],
-    },
-    fontSize: {
-        base: ['16px', '24px'],
+      sans: ["Poppins", "sans-serif"],
     },
     extend: {
-        colors: {
-            primary: {
-                light: '#131417',
-                DEFAULT: '#131417',
-                dark: '#F2F2F2',
-            },
-            secondary: {
-                light: '#F2F2F2',
-                DEFAULT: '#F2F2F2',
-                dark: '#131417',
-            },
-        },
+      colors: {
+        primary: "#131417",
+        secondary: "#F2F2F2",
+      },
     },
   },
   plugins: [],
-}
-
+};
